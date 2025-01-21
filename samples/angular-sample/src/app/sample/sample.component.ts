02101cd8d58ceb06163584511c0e80a6a93e5f79
adams85 <31276480+adams85@users.noreply.github.com>
2025-01-21 16:10:35 +0100

    Add sample implementation of getAllValues to Angular sample (#11)

--- samples/angular-sample/src/app/sample/sample.component.ts
+++ samples/angular-sample/src/app/sample/sample.component.ts
@@ -1,9 +1,9 @@
 import { AsyncPipe } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { type SettingValue, User } from "@configcat/sdk/browser";
+import { BehaviorSubject, distinctUntilChanged, Observable, switchMap } from "rxjs";
 import { ConfigCatService } from "../configcat.service";
-import { Observable } from "rxjs";
-import { User } from "@configcat/sdk/browser";
 
 @Component({
   selector: "app-sample",
@@ -13,12 +13,24 @@ import { User } from "@configcat/sdk/browser";
   imports: [AsyncPipe, FormsModule]
 })
 export class SampleComponent {
-  isAwesomeEnabled$: Observable<boolean>
+  isAwesomeEnabled$: Observable<boolean>;
   isPOCEnabled?: boolean;
-  userEmail = "configcat@example.com";
+  allKeyValues$: Observable<Map<string, SettingValue>>;
+
+  private userEmailSubject = new BehaviorSubject<string>("configcat@example.com");
+  get userEmail() { return this.userEmailSubject.value; }
+  set userEmail(value: string) { this.userEmailSubject.next(value); }
 
   constructor(private configCatService: ConfigCatService) {
     this.isAwesomeEnabled$ = this.configCatService.getValue("isAwesomeFeatureEnabled", false);
+
+    this.allKeyValues$ = this.userEmailSubject.pipe(
+      distinctUntilChanged(),
+      switchMap(userEmail => {
+        const userObject = new User("#SOME-USER-ID#", userEmail);
+        return this.configCatService.getAllValues(userObject);
+      })
+    );
   }
 
   async checkProofOfConcept(): Promise<void> {
