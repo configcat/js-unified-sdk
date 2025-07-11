ca30d66df20a8e084935eb5da24aba8665efeb94
Adam Simon <adamosimoni@gmail.com>
2025-07-11 22:05:09 +0200

    Add documentation to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,37 @@
 "use strict";
 
+/*
+
+This is the project's build script. It's a plain Node.js program that uses Gulp to orchestrate the build process.
+(In case you need to dig into it deeper, a VS Code debug configuration named "Debug build" is available.)
+
+The TARGETS array below specifies the build outputs (or "builds" for short). We have two types of builds:
+- lib - separate module files for bundlers and runtimes like Node.js
+- dist - pre-bundled script files that can be imported in browsers directly
+
+The build process is very simple, consisting of the following tasks:
+1. clean - removes previous build artifacts if any
+2. compile - runs tsc for an item of TARGETS with the specified tsconfig
+3. postProcess - makes adjustments to the output of compile if necessary
+
+The following post-processing steps are performed:
+- Adjust .d.ts files of the CJS build - According to our tests, the best compatibility with various build tools can
+  be achieved when each source file has the corresponding .d.ts file in the same directory. However, we don't want to
+  duplicate the type definitions because that could lead to other subtle issues, so we replace the content of the CJS
+  build's .d.ts files so they just reexport the type definitions from the corresponding files of the ESM build.
+- Fix extensions of referenced files in export/import statements - Most runtimes require explicit file extensions in
+  import statements, so we add the .js extension to the import paths.
+- Strip const from enums to provide normal enums to consumers - We use const enums internally to reduce bundle size but
+  also want to preserve enum mappings for consumers. Therefore, we enable "preserveConstEnums" in tsconfig, but that's
+  not enough in itself: we also need to strip const from enum declarations in .d.ts files.
+- Add package.json to the ESM build - Since the package type is commonjs, we need to create another package.json file
+  with "type": "module" in the lib/esm directory, otherwise modules with .js extension won't be recognized as ES modules
+  by Node.js.
+- Inject package version - We need to replace the "CONFIGCAT_SDK_VERSION" magic string with the actual version string
+  specified in the package.json.
+
+*/
+
 const child_process = require("child_process");
 const fs = require("fs"), fsp = fs.promises;
 const glob = require("glob");
@@ -51,7 +83,9 @@ function compile(targetId, { useWebpack, config }) {
 async function postProcess(targetId, targetFile, targetDir, { importExtension, reexportTypesFrom, skipTypes, addModulePackageJson, skipVersionUpdate }, version) {
   console.log(`* Post-processing target '${targetId}'...`);
 
-  if (targetDir != null && (importExtension || reexportTypesFrom != null)) {
+  if (targetDir != null // no need to post-process bundles at the moment
+    && (importExtension || reexportTypesFrom != null)
+  ) {
     const importDir = reexportTypesFrom != null ? path.resolve(targetDir, reexportTypesFrom) : null;
     for await (const file of glob.globIterate(normalizePathSeparator(targetDir) + "/**", { absolute: true })) {
       const isDts = file.endsWith(".d.ts");
@@ -59,10 +93,7 @@ async function postProcess(targetId, targetFile, targetDir, { importExtension, r
         if (skipTypes) continue;
 
         if (reexportTypesFrom != null) {
-          // According to our tests, the best compatibility with various build tools can be achieved when each source file has
-          // the corresponding .d.ts file in the same directory. However, we don't want to duplicate the type definitions
-          // (because that could lead to other subtle issues), so we replace the content of the CJS build's .d.ts files so
-          // they just reexport the type definitions from the corresponding files of the ESM build.
+          // Adjust .d.ts files of the CJS build
           const importDirRelative = path.relative(path.dirname(file), importDir);
           let importFile = path.join(importDirRelative, path.relative(targetDir, file));
           importFile = changeExtension(changeExtension(importFile, ""), importExtension ?? "");
@@ -70,7 +101,7 @@ async function postProcess(targetId, targetFile, targetDir, { importExtension, r
           await fsp.writeFile(file, fileContent, "utf8", { flush: true });
           continue;
         } else {
-          // Strip const from enums to provide normal enums to consumers.
+          // Strip const from enums to provide normal enums to consumers
           let fileContent = await fsp.readFile(file, "utf8");
           fileContent = fileContent.replace(/declare\s+const\s+enum/g, () => {
             return "declare enum";
@@ -85,7 +116,7 @@ async function postProcess(targetId, targetFile, targetDir, { importExtension, r
         continue;
       }
 
-      // Fix extensions of referenced files in export/import statements.
+      // Fix extensions of referenced files in export/import statements
       let fileContent = await fsp.readFile(file, "utf8");
       const isEsm = addModulePackageJson || isDts;
       fileContent = (isEsm ? fixEsmImports : fixCjsImports)(path.dirname(file), fileContent, importExtension);
@@ -95,13 +126,12 @@ async function postProcess(targetId, targetFile, targetDir, { importExtension, r
 
   const outputPath = targetFile ?? targetDir;
 
-  // As the package type is commonjs, we need to add another package json to the directory containing the ES module files,
-  // otherwise the .js extension won't work for them.
+  // Add package.json to the ESM build
   if (addModulePackageJson) {
     await fsp.writeFile(path.join(outputPath, "package.json"), '{ "type": "module", "sideEffects": false }', { flush: true });
   }
 
-  // We need to replace the "CONFIGCAT_SDK_VERSION" magic string with the actual version string specified in the package.json.
+  // Inject package version
   if (!skipVersionUpdate) {
     const versionFilePaths = targetFile != null
       ? [targetFile, changeExtension(targetFile, ".min" + path.extname(targetFile))]
