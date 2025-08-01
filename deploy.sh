e6d7a8913567b8be00bc1b6020be773653c9f9ec
Adam Simon <adamosimoni@gmail.com>
2025-08-01 20:08:40 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#Run this script to update @configcat/sdk to the latest and release a new version of @configcat/sdk
-set -e #Making sure script stops on error
-npm test
-git push origin $(npm version patch)
-git push
